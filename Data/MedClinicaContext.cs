3fcbae22cf36a943f4720e2577f321eef357fcb4
vinixp89 <vsantos-90@hotmail.com>
2024-01-07 20:12:50 -0300

    testando as controllers

---
+++ Data/MedClinicaContext.cs
@@ -0,0 +1,22 @@
+﻿using MedClinica.WebAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MedClinica.WebAPI.Data
+{
+    public class MedClinicaContext : DbContext
+    {
+        public DbSet<Medico> Medicos { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Especialidade> Especialidades { get; set; }
+        public DbSet<PacienteEspecialidade> PacienteEspecialidades { get; set; }
+
+        public MedClinicaContext(DbContextOptions<MedClinicaContext> options) :base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder builder) 
+        {
+            builder.Entity<PacienteEspecialidade>().HasKey(AD => new { AD.PacienteId, AD.EspecialidadeId });
+        
+        
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -2,10 +2,12 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using MedClinica.WebAPI.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -25,6 +27,9 @@ namespace  MedClinica.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MedClinicaContext>(
+                context => context.UseSqlite(Configuration.GetConnectionString("Default"))
+                );
             services.AddControllers();
         }
 
